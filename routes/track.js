20412b880745e25eabc1ef6245851f9455a337ad
Brendan Telzrow <telzrowb@gmail.com>
2016-03-14 18:00:00 -0500

    add tracking template

--- routes/track.js
+++ routes/track.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Orders = require('../models').Order;
 
 router.get('/', function(req, res, next){
     res.render('track');
+});
+
+router.post('/', function(req, res, next){
+    var id = req.sanitize(req.body.uuid).trim();
+    Orders.find({where: {uuid: id}}).then(function(result){
+        res.json(result.dataValues);
+    });
 });
 
 module.exports = router;
